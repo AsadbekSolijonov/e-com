f4e9cb15b53f1d84a3101d35947fe323a06a32b4
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2024-08-19 13:30:17 +0500

    adada

--- root/settings.py
+++ root/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(5hercw7h68wzgu5g368i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', [])
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', []), ]
 
 # Application definition
 
